2f1dd5e6246b087e97938cb057463d90d9d4005e
Newman Chow <newman@oursky.com>
2023-08-16 00:48:19 +0800

    Try to make layout less ugly

--- app/src/components/MainNav.tsx
+++ app/src/components/MainNav.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link"
 
 import { cn } from "@/lib/utils/ui"
+import logoImage from '@/assets/logo.png'
 import { FormattedMessage } from 'react-intl'
 import { Routes } from 'generated'
 import React, { type PropsWithChildren } from 'react'
 import { type Url } from 'url';
+import Image from 'next/image'
 
 const MainNavLink: React.FC<PropsWithChildren & { href: Url | string }> = ({
   href,
@@ -28,9 +30,12 @@ export function MainNav({
 }: React.HTMLAttributes<HTMLElement>) {
   return (
     <nav
-      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
+      className={cn("px-4 w-full bg-opacity-30 bg-white rounded-2xl flex items-center space-x-4 lg:space-x-6", className)}
       {...props}
     >
+      <MainNavLink href={Routes.IndexPage()}>
+        <Image className="rounded-full" src={logoImage} alt="logo" width={64} height={64} />
+      </MainNavLink>
       <MainNavLink href={Routes.IndexPage()}>
         <FormattedMessage id="mainnav.link.all" defaultMessage="全部蛋糕" />
       </MainNavLink>
--- app/src/layouts/Storefronts.tsx
+++ app/src/layouts/Storefronts.tsx
@@ -4,8 +4,8 @@ import React, { type PropsWithChildren, Suspense } from 'react';
 const StorefrontLayout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className="flex h-screen flex-col bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
-      <div className="flex flex-shrink-0 h-[76px] items-center px-4">
-        <MainNav className="mx-6" />
+      <div className="flex flex-shrink-0 h-[100px] items-center px-4">
+        <MainNav className="sm:mx-6" />
         <div className="ml-auto flex items-center space-x-4">
         </div>
       </div>
