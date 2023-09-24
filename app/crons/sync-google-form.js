fd490096a3895a0b30f877c0138db0d8bc5185fb
Newman Chow <newman@oursky.com>
2023-09-25 01:31:27 +0800

    Move scheduler to separate deployment

--- app/crons/sync-google-form.js
+++
@@ -1,6 +0,0 @@
-const cron = require('node-cron');
-
-const enqueueSyncGoogleForm = () => fetch(process.env.API_URL + '/api/jobs/sync-google-form')
-
-enqueueSyncGoogleForm();
-cron.schedule('*/10 * * * *', enqueueSyncGoogleForm);
