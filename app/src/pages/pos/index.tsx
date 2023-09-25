5bfaa11065a8740590a04e0de3a0c13e0864294a
Newman Chow <newman@oursky.com>
2023-09-25 09:51:55 +0800

    Fix pos page not loaded

--- app/src/pages/pos/index.tsx
+++ app/src/pages/pos/index.tsx
@@ -1,7 +1,20 @@
+import { createServerSideHelpers } from '@/lib/createServerSideHelpers';
 import { NextPageWithLayout } from '@/lib/types';
 import HomeScreen from '@/modules/pos/screens/Home.screen';
 import { type NextPage, type GetServerSidePropsContext } from 'next';
 import { useRouter } from 'next/router';
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext
+) {
+  const serverSideHelpers = await createServerSideHelpers(context);
+
+  return {
+    props: {
+      trpcState: serverSideHelpers.dehydrate(),
+    }
+  }
+}
 
 const PosHomePage: NextPage = () => {
   const router = useRouter()
