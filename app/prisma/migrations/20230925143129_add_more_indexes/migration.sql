02762a57d14a92ef6920736de5aad29160cf8075
Newman Chow <newman@oursky.com>
2023-09-25 22:58:56 +0800

    Add more indexes

---
+++ app/prisma/migrations/20230925143129_add_more_indexes/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "deliveryDate" ON "Order"("deliveryDate");
+
+-- CreateIndex
+CREATE INDEX "phoneNumber" ON "Order"("phoneNumber");
+
+-- CreateIndex
+CREATE INDEX "socialHandle" ON "Order"("socialHandle");
